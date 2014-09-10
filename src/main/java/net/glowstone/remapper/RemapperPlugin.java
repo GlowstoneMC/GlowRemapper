f0572046ffdeb8d8924a9e40662141c922d31763
Tad Hardesty <tad@platymuus.com>
2014-09-10 00:21:22 -0500

    Intial commit.

---
+++ src/main/java/net/glowstone/remapper/RemapperPlugin.java
@@ -0,0 +1,15 @@
+package net.glowstone.remapper;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * The main class for GlowRemapper.
+ */
+public class RemapperPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
