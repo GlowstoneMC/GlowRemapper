609c8f734131ef4e62eb679f152bc352a2ef7357
Tad Hardesty <tad@platymuus.com>
2014-09-13 23:26:58 -0500

    Also remap calls to methods which have been remapped.

--- src/main/java/net/glowstone/remapper/RemapClassVisitor.java
+++ src/main/java/net/glowstone/remapper/RemapClassVisitor.java
@@ -28,20 +28,42 @@ public class RemapClassVisitor extends ClassVisitor {
         super.visit(version, access, name, signature, superName, interfaces);
     }
 
-    @Override
-    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-        // try to find a matching mapping
+    private Mapping find(String className, String name, String desc) {
         for (Mapping mapping : mappings) {
             if (name.equals(mapping.getMethodName()) &&
                     desc.equals(mapping.getMethodSignature()) &&
-                    parents.isParent(currentClass, mapping.getClassName())) {
+                    parents.isParent(className, mapping.getClassName())) {
                 mapping.addUsage();
-                name = mapping.getNewName();
-                access |= Opcodes.ACC_SYNTHETIC;
-                break;
+                return mapping;
             }
         }
+        return null;
+    }
 
-        return super.visitMethod(access, name, desc, signature, exceptions);
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        // try to find a matching mapping
+        Mapping mapping = find(currentClass, name, desc);
+        if (mapping != null) {
+            name = mapping.getNewName();
+            access |= Opcodes.ACC_SYNTHETIC;
+        }
+        return new RemapperMethodVisitor(super.visitMethod(access, name, desc, signature, exceptions));
+    }
+
+    private class RemapperMethodVisitor extends MethodVisitor {
+        public RemapperMethodVisitor(MethodVisitor methodVisitor) {
+            super(Opcodes.ASM4, methodVisitor);
+            // ASM4 used intentionally - overrides older API method
+        }
+
+        @Override
+        public void visitMethodInsn(int opcode, String owner, String name, String desc) {
+            Mapping mapping = find(owner, name, desc);
+            if (mapping != null) {
+                name = mapping.getNewName();
+            }
+            super.visitMethodInsn(opcode, owner, name, desc);
+        }
     }
 }
